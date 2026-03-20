908cfcd95c27ec225e7a529ae69ead72acd96a30
GlobalCodes <glorychukwuzurum@gmail.com>
2026-03-20 03:12:01 +0000

    fix: update API URL to point to the correct backend

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   const [todos, setTodos] = useState([]);
   const [newTodo, setNewTodo] = useState('');
 
-  const url = 'https://mern-assessment-backend-p4kd.onrender.com'
+  const url = 'https://rebase-mern-assessment-backend.onrender.com'
 
   useEffect(() => {
     fetch(`${url}/api/todos`)
